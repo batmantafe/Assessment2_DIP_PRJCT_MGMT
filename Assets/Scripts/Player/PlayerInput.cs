d065f3e5bc76ceeb1c5a92cf05241267cc11f66c
Duane Reymond <duane.reymond@gmail.com>
2018-03-15 13:38:22 +1100

    PlayerInput script: Jump done.

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    [Header("Movement")]
     public Rigidbody playerRigi;
     public float moveSpeed;
+
+    [Header("Jump")]
+    public float jumpThrust;
+    public float jumpTimer;
+    public float jumpTimerMax;
 
     // Use this for initialization
     void Start()
     {
         playerRigi = GetComponent<Rigidbody>();
         moveSpeed = 5f;
+
+        jumpThrust = 50f;
+        jumpTimerMax = .2f;
+        jumpTimer = jumpTimerMax;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         PlayerMovement();
+        PlayerJump();
     }
 
+    #region Movement
     void PlayerMovement()
     {
         // Move Up
@@ -46,4 +58,44 @@ public class PlayerInput : MonoBehaviour
             playerRigi.MovePosition(transform.position + transform.right * (Time.deltaTime * moveSpeed));
         }
     }
+    #endregion
+
+    void PlayerJump()
+    {
+        if (Input.GetKey(KeyCode.Space) && jumpTimer != 0f)
+        {
+            JumpTimerFunction();
+
+            playerRigi.AddForce(transform.up * jumpThrust);
+        }
+    }
+
+    void JumpTimerFunction()
+    {
+        jumpTimer = jumpTimer - (1 * Time.deltaTime);
+
+        if (jumpTimer <= 0f)
+        {
+            jumpTimer = 0f;
+        }
+    }
+
+    #region Collisions
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            // Reset Clock
+            jumpTimer = jumpTimerMax;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+
+        }
+    }
+    #endregion
 }
