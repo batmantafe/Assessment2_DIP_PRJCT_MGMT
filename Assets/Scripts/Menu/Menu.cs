8bfd0c6a4576c395551eef9a441ac6c355b8c8ad
Duane Reymond <duane.reymond@gmail.com>
2018-03-30 09:19:56 +1100

    Added Splash Screen, Main Menu, and Character Scenes.

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    private string currentScene;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Splash();
+    }
+
+    #region Splash Screen
+    void Splash()
+    {
+        if (currentScene == "Splash")
+        {
+            if (Input.anyKey)
+            {
+                SceneManager.LoadScene("Main Menu");
+            }
+        }
+    }
+    #endregion
+
+    #region Main Menu
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Character");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+    #endregion
+
+    #region Character
+    public void DiverButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ShrimpButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    #endregion
+}
